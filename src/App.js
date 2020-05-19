3d96dbfd6b03faea9835a03d2962d025f3ea2d28
antorpo <antonio.gonzalezr@udea.edu.co>
2020-05-19 13:34:56 -0500

    Se implementa el uso de useEffect con segundo parametro igual a  [] que equivale a solo usar el efecto cuando el componente se renderice por primera vez como lo hace el componentDidMount.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import React from 'react'
 import { ListCategories } from './components/ListCategories'
 import { ListPhotoCard } from './components/ListPhotoCard'
 import { Logo } from './components/Logo'
-import { GlobalStyle } from './globalStyles'
-
-
-// const PHOTOCARD_ELEMENT = {
-//   id: 0,
-//   categoryId: 2,
-//   src: 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png',
-//   userId: 100,
-//   likes: 7
-// }
+import { GlobalStyle } from './styles/globalStyles'
 
 const App = () => {
   return (
--- src/components/ListCategories/index.js
+++ src/components/ListCategories/index.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+import { categories as categoriesEndPoint } from '../../../endpoints.json'
 
 export const ListCategories = () => {
+  const [categories, setCategories] = useState({
+    data: [],
+    loading: true,
+    error: null
+  })
+
+  // // Sin usar async/await por problemas con Babel 7.
+  useEffect(() => {
+    setCategories({
+      loading: true,
+      error: null
+    })
+
+    try {
+      axios.get(categoriesEndPoint).then((res) => {
+        setCategories({
+          loading: false,
+          data: res.data
+        })
+      })
+    } catch (error) {
+      setCategories({
+        loading: false,
+        error: error
+      })
+    }
+  }, []) // El array vacio le indica que solo se llamara al montarse el componente es decir el primer renderizado. (componentDidMount)
+
+  if (categories.loading) {
+    return <h3>Loading...</h3>
+  }
+
+  if (categories.error) {
+    return <h3>{`Error: ${categories.error}`}</h3>
+  }
+
   return (
     <List>
-      {categories.map((_category) => (
+      {categories.data.map((_category) => (
         <Item key={_category.id}>
           <Category {..._category} />
         </Item>

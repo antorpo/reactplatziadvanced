dfe4abfe0cbc518f8055142b68fbc3d60d944d39
antorpo <antonio.gonzalezr@udea.edu.co>
2020-06-29 18:01:32 -0500

    Se crean mas render props como mutaciones para el proceso de login y registro mediante
    webtokens los cuales se guardan dentro de sessionStorage para tener una persistencia de la
    sesion. Ademas se hace uso del hook useContext que nos permite utilizar de manera mas comoda
    los contextos nativos de React.

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,13 +1,60 @@
-import React from 'react'
-import Context from '../Context'
+import React, { useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/UserForm'
+import { RegisterMutation } from '../container/RegisterMutation'
+import { LoginMutation } from '../container/LoginMutation'
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {({ activateAuth }) => {
-      return (
-        <UserForm onSubmit={activateAuth} />
-      )
-    }}
-  </Context.Consumer>
-)
+export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
+
+  return (
+    <>
+      <RegisterMutation>
+        {(register, { data, loading, error }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            register({ variables }).then(({ data }) => {
+              const { signup } = data
+              activateAuth(signup)
+            })
+          }
+
+          const errorMsg = error && 'El usuario ya existe!'
+
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              onSubmit={onSubmit}
+              title='Registrarse'
+            />
+          )
+        }}
+      </RegisterMutation>
+
+      <LoginMutation>
+        {(login, { data, error, loading }) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password }
+            const variables = { input }
+            login({ variables }).then(({ data }) => {
+              const { login } = data
+              activateAuth(login)
+            })
+          }
+
+          const errorMsg = error && 'Credenciales incorrectas!'
+          return (
+            <UserForm
+              disabled={loading}
+              error={errorMsg}
+              onSubmit={onSubmit}
+              title='Iniciar Sesion'
+            />
+          )
+        }}
+      </LoginMutation>
+    </>
+  )
+}

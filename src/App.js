2817f3516cceaf9b2ba0fbd2b91ba22cdf428f21
antorpo <antonio.gonzalezr@udea.edu.co>
2020-06-30 17:12:04 -0500

    Correcion de rutas y se usa el context con su respectivo hook.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { GlobalStyle } from './styles/globalStyles'
-import { Router } from '@reach/router'
+import { Router, Redirect } from '@reach/router'
 import { NavBar } from './components/NavBar'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
-import Context from './Context'
+import { Context } from './Context'
+import { NotFound } from './pages/NotFound'
 
 const App = () => {
   // Obtener parametros usando QueryString y URLParams
   // const urlParams = new window.URLSearchParams(window.location.search)
   // const detailId = urlParams.get('detail')
 
-  const UserLogged = ({ children }) => {
-    return children({ isAuth: false })
-  }
+  const { isAuth } = useContext(Context)
 
   return (
     <>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:id' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' />}
+        {!isAuth && <Redirect from='/user' to='/login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth ? (
-            <Router>
-              <Favs path='/favs' />
-              <User path='/user' />
-            </Router>
-          ) : (
-            <Router>
-              <NotRegisteredUser path='/favs' />
-              <NotRegisteredUser path='/user' />
-            </Router>
-          )}
-      </Context.Consumer>
       <NavBar />
     </>
   )
---
+++ src/pages/NotFound.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const NotFound = () => <h1>Error 404</h1>
